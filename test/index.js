f3ffc8a54837b9231b321a8fe5dd6eca2811aeaa
vanetix <matmcfarland@gmail.com>
2013-03-07 22:58:22 -0600

    add some simple tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,17 @@ var grunt = require('grunt');
 
 exports.mapper = {
   basic: function(test) {
-    test.expect(1);
+    var actual, expected;
+
+    test.expect(2);
+    
+    actual = grunt.file.read('tmp/cats.html');
+    expected = grunt.file.read('test/expected/cats.html');
+    test.equal(actual, expected, 'should correctly map html assets');
+
+    actual = grunt.file.read('tmp/cats.css');
+    expected = grunt.file.read('test/expected/cats.css');
+    test.equal(actual, expected, 'should correctly map css assets');
 
     test.done();
   }
